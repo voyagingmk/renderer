0148cf36c5b9e8e1da9f10ddfeb9ff6426e18759
unknown <>
2015-12-15 23:12:55 +0800

    no message

--- include/transform.hpp
+++ include/transform.hpp
@@ -4,6 +4,28 @@
 #include "base.hpp"
 
 namespace renderer {
+
+	template<class V, const int M, const int N>
+	class MxN {
+	public:
+		typedef V ValueType;
+		static const int row = M;
+		static const int col = N;
+		V data[M * N];
+	public:
+		MxN() {
+			memset(data, 0, row * col * sizeof(V));
+		}
+		MxN(std::initializer_list<V> values) {
+			int idx = 0;
+			for (auto& value : values) {
+				data[idx++] = value;
+			}
+		}
+		const V& operator [] (const int i) { return data[i]; }
+	};
+
+
 	template<class T>
 	class Matrix final {
 	public:
@@ -26,6 +48,12 @@ namespace renderer {
 			auto pool = GetPool<T>();
 			pool->deleteElement(data);
 		}
+		inline int row() {
+			return T::row;
+		}
+		inline int col() {
+			return T::col;
+		}
 		//access only
 		inline const V& operator [] (const int i) { return static_cast<V>(*((V*)data + i)); }
 		//API
@@ -33,11 +61,11 @@ namespace renderer {
 			Matrix<T> result;
 			int row = T::row, col = T::col;
 			int idx;
-			V* data = result.data->data;
+			V* dataOfResult = result.data->data;
 			for (int i = 0; i < row; i++) {
 				for (int j = 0; j < col; j++) {
 					idx = i * col + j;
-					data[idx] = (*this)[idx] + m[idx];
+					dataOfResult[idx] = (*this)[idx] + m[idx];
 				}
 			}
 			return result;
@@ -46,14 +74,47 @@ namespace renderer {
 			Matrix<T> result;
 			int row = T::row, col = T::col;
 			int idx;
-			V* data = result.data->data;
+			V* dataOfResult = result.data->data;
 			for (int i = 0; i < row; i++) {
 				for (int j = 0; j < col; j++) {
 					idx = i * col + j;
-					data[idx] = (*this)[idx] - m[idx];
+					dataOfResult[idx] = (*this)[idx] - m[idx];
 				}
 			}
 			return result;
+		}
+		template<int M, int N>
+		Matrix<T> multiply(Matrix<MxN<V, M, N>>& m) {
+			Matrix<MxN<V, T::row, N>> result;
+			int row = T::row, col = T::col;
+			int idx;
+			V* dataOfResult = result.data->data;
+			for (int i = 0; i < m.row(); i++) {
+				for (int j = 0; j < m.col(); j++) {
+					V total = 0;
+					for (int k = 0; k < col; k++) {
+						total += (*this)[i * col + k] * m[k * m.col() + j];
+					}
+					dataOfResult[i * m.col() + j] = total;
+				}
+			}
+			return result;
+		}
+		Matrix<T> multiply(V v) {
+			Matrix<T> result;
+			int row = T::row, col = T::col;
+			int idx;
+			V* dataOfResult = result.data->data;
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
+					idx = i * col + j;
+					dataOfResult[idx] = (*this)[idx] *v;
+				}
+			}
+			return result;
+		}
+		Matrix<T> divide(T m) {
+			return multiply(T(1.0)/m);
 		}
 		//debug
 		void debug() {
@@ -69,25 +130,6 @@ namespace renderer {
 		}
 	};
 
-	template<class V, const int M, const int N>
-	class MxN {
-	public:
-		typedef V ValueType;
-		static const int row = M;
-		static const int col = N;
-		V data[M * N];
-	public:
-		MxN() {
-			memset(data, 0, row * col * sizeof(V));
-		}
-		MxN(std::initializer_list<V> values) {
-			int idx = 0;
-			for (auto& value : values) {
-				data[idx++] = value;
-			}
-		}
-		const V& operator [] (const int i) { return data[i]; }
-	};
 
 	typedef MxN<float, 4, 4> Matrix4x4Value;
 	typedef Matrix<Matrix4x4Value> Matrix4x4;
