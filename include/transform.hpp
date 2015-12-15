b2ca346e16ebb77728ed2f8eda16f8701dde8418
unknown <>
2015-12-15 21:57:15 +0800

    no message

--- include/transform.hpp
+++ include/transform.hpp
@@ -6,6 +6,7 @@
 namespace renderer {
 	template<class T>
 	class Matrix final {
+	public:
 		T * data;
 		typedef typename T::ValueType V;
 	public:
@@ -15,9 +16,32 @@ namespace renderer {
 		Matrix(std::initializer_list<V> values) {
 			data = GetPool<T>()->newElement(T(std::forward<std::initializer_list<V>>(values)));
 		}
+		Matrix(Matrix<T>&& m) {
+			data = m.data;
+			m.data = nullptr;
+		}
 		virtual ~Matrix() {
+			if (!data)
+				return;
 			auto pool = GetPool<T>();
 			pool->deleteElement(data);
+		}
+		//access only
+		inline const V& operator [] (const int i) { return static_cast<V>(*((V*)data + i)); }
+		//API
+		Matrix<T> add(Matrix<T>& m);
+		Matrix<T> minus(Matrix<T>& m);
+		//debug
+		void debug() {
+			int row = T::row, col = T::col;
+			int idx;
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < col; j++) {
+					idx = i * col + j;
+					printf("%.1f,", (*this)[idx]);
+				}
+			}
+			printf("\n");
 		}
 	};
 
@@ -25,16 +49,20 @@ namespace renderer {
 	class MxN {
 	public:
 		typedef V ValueType;
-		static const int m = M;
-		static const int n = N;
+		static const int row = M;
+		static const int col = N;
 		V data[M * N];
 	public:
+		MxN() {
+			memset(data, 0, row * col * sizeof(V));
+		}
 		MxN(std::initializer_list<V> values) {
 			int idx = 0;
 			for (auto& value : values) {
 				data[idx++] = value;
 			}
 		}
+		const V& operator [] (const int i) { return data[i]; }
 	};
 
 	typedef MxN<float, 4, 4> Matrix4x4Value;
--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ using namespace renderer;
 int main(int argc, char ** argv){
 	Parser parser;
 	//parser.parseFromFile("config.json");
-	Matrix4x4  m ={ 1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8 };
+	Matrix4x4  m1 = { 1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8 };
+	Matrix4x4  m2 = { 1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8 };
+	Matrix4x4  m3 = m1.add(m2);
+	m3.debug();
 	return 0;
 }
