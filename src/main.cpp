f193ac711c25d0c45fb4e17b6d22bbf8bd4d80fe
unknown <>
2015-12-15 23:13:35 +0800

    no message

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,20 @@ using namespace renderer;
 int main(int argc, char ** argv){
 	Parser parser;
 	//parser.parseFromFile("config.json");
-	Matrix4x4  m1 = { 1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8 };
-	Matrix4x4  m2 = { 1,2,3,4,5,6,7,8,1,2,3,4,5,6,7,8 };
-	Matrix4x4  m3 = m1.add(m2);
-	Matrix4x4  m4 = m3.minus(m2);
+	Matrix4x4 m1 = { 
+		1,0,0,0,
+		0,1,0,0,
+		0,0,1,0,
+		0,0,0,1 };
+	Matrix4x4 m2 = { 
+		1,2,3,4,
+		5,6,7,8,
+		1,2,3,4,
+		5,6,7,8 };
+	Matrix4x4 m3 = m1.add(m2);
+	Matrix4x4 m4 = m3.minus(m2);
 	m4.debug();
-
+	auto m5 = m1.multiply(m2.multiply(3.0f));
+	m5.debug();
 	return 0;
 }
