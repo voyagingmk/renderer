f8da838ceaa9874bdb4257bc6de0ff1e5de9f9ac
wyman <voyagingmk@gmail.com>
2017-10-02 22:43:39 +0800

    no message

--- src/system/uiSys.cpp
+++ src/system/uiSys.cpp
@@ -18,22 +18,17 @@ namespace renderer {
     void UISystem::receive(const DrawUIEvent &evt) {
         auto com = m_objMgr->getSingletonComponent<SDLContext>();
         ImGui_ImplSdlGL3_NewFrame(com->win);
-        
-        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
         // 1. Show a simple window
         // Tip: if we don't call ImGui::Begin()/ImGui::End() the widgets appears in a window automatically called "Debug"
         {
             static float f = 0.0f;
+            ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiCond_FirstUseEver);
             ImGui::Text("Hello, world!");
             ImGui::SliderFloat("float", &f, 0.0f, 1.0f);
-            ImGui::ColorEdit3("clear color", (float*)&clear_color);
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
         }
         // Rendering
-        auto s = ImGui::GetIO().DisplaySize;
         glViewport(0, 0, (int)ImGui::GetIO().DisplaySize.x, (int)ImGui::GetIO().DisplaySize.y);
-        // glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        // glClear(GL_COLOR_BUFFER_BIT);
         ImGui::Render();
     }
     
