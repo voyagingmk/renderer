58abebd8d0485a35ed6caa07a3ddf6e8d616bd60
Wyman-PC <voyagingmk@gmail.com>
2017-10-14 17:54:05 +0800

    可以生成ssao图

--- src/system/renderSys.cpp
+++ src/system/renderSys.cpp
@@ -19,79 +19,6 @@
 
 using namespace std;
 
-// renderCube() renders a 1x1 3D cube in NDC.
-// -------------------------------------------------
-unsigned int cubeVAO = 0;
-unsigned int cubeVBO = 0;
-void renderCube()
-{
-	// initialize (if necessary)
-	if (cubeVAO == 0)
-	{
-		float vertices[] = {
-			// back face
-			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,  // bottom-left
-			1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,  // top-right
-			1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,  // bottom-right         
-			1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,  // top-right
-			-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f, // bottom-left
-			-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,  // top-left
-			// front face
-			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,  // bottom-left
-			1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f, // bottom-right
-			1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f, // top-right
-			1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,// top-right
-			-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,  // top-left
-			-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,  // bottom-left
-			// left face
-			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, // top-right
-			-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f,// top-left
-			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f, // bottom-left
-			-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f,  // bottom-left
-			-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f, // bottom-right
-			-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f, // top-right
-			// right face
-			1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, // top-left
-			1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  // bottom-right
-			1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  // top-right         
-			1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f, // bottom-right
-			1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f, // top-left
-			1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f, // bottom-left     
-			// bottom face
-			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,  // top-right
-			1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,  // top-left
-			1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,  // bottom-left
-			1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f, // bottom-left
-			-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,// bottom-right
-			-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f, // top-right
-			// top face
-			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f,  // top-left
-			1.0f,  1.0f , 1.0f,  0.0f,  1.0f,  0.0f,// bottom-right
-			1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, // top-right     
-			1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f, // bottom-right
-			-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f, // top-left
-			-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,  // bottom-left        
-		};
-		glGenVertexArrays(1, &cubeVAO);
-		glGenBuffers(1, &cubeVBO);
-		// fill buffer
-		glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-		// link vertex attributes
-		glBindVertexArray(cubeVAO);
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-		glBindBuffer(GL_ARRAY_BUFFER, 0);
-		glBindVertexArray(0);
-	}
-	// render Cube
-	glBindVertexArray(cubeVAO);
-	glDrawArrays(GL_TRIANGLES, 0, 36);
-	glBindVertexArray(0);
-}
-
 namespace renderer {
     void RenderSystem::init(ObjectManager &objMgr, EventManager &evtMgr) {
         printf("RenderSystem init\n");
@@ -213,8 +140,7 @@ namespace renderer {
 			CheckGLError; 
 			m_evtMgr->emit<UploadMatrixToShaderEvent>(obj, shader);
 			CheckGLError; 
-		//	m_evtMgr->emit<DrawMeshBufferEvent>(obj);
-			renderCube();
+			m_evtMgr->emit<DrawMeshBufferEvent>(obj);
 			CheckGLError;
 			m_evtMgr->emit<DeactiveMaterialEvent>(matCom->settingID);
 			CheckGLError;
