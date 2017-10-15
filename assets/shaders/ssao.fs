d9d9942533fc4ca9cc349d5f0b326f7737286de4
Wyman-PC <voyagingmk@gmail.com>
2017-10-15 17:08:33 +0800

    no message

--- assets/shaders/ssao.fs
+++ assets/shaders/ssao.fs
@@ -13,7 +13,7 @@ uniform vec3 samples[64];
 
 // parameters (you'd probably want to use them as uniforms to more easily tweak the effect)
 int kernelSize = 64;
-float radius = 0.5;
+float radius = 1.0;
 float bias = 0.025;
 
 
